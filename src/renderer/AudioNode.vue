ff9b4d8609eaf1a6432d5dd54dbdd5977ee8ebc4
ntropish <ntropish@gmail.com>
2019-03-16 13:54:33 -0500

    Add missing file

---
+++ src/renderer/AudioNode.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="root">
+    <div v-if="node.type === 'track'">Track: {{ node.data.track }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    node: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({})
+};
+</script>
+
+<style scoped>
+.root {
+  /* padding: 0.4em; */
+  background: hsla(0, 0%, 70%, 0.2);
+  position: absolute;
+  pointer-events: none;
+}
+</style>
+
