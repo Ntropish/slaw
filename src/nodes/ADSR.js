15c714392b8a8154ae70218d2eabb5d6656c69c9
ntropish <ntropish@gmail.com>
2019-03-23 00:12:53 -0500

    Add stop method to ASDR

--- src/nodes/ADSR.js
+++ src/nodes/ADSR.js
@@ -2,11 +2,11 @@ import { connect } from './util'
 
 export default function ADSRFactory({ context, bps }) {
   const gainNode = context.createGain()
-  const asdr = [0.1, 0.3, 0.1, 0.2]
+  const adsr = [0.1, 0.3, 0.1, 0.2]
 
   function onEvent({ beats, time }) {
     const noteEnd = time + beats / bps
-    const [a, d, s, r] = asdr
+    const [a, d, s, r] = adsr
 
     gainNode.gain.setValueAtTime(0, time)
     // Math min to make sure gain doesn't come in after note should be stopping
@@ -16,8 +16,15 @@ export default function ADSRFactory({ context, bps }) {
     gainNode.gain.linearRampToValueAtTime(0, noteEnd + r)
   }
 
+  function stop(_time) {
+    const time = _time || context.getOutputTimestamp().contextTime
+    gainNode.gain.cancelScheduledValues(time)
+    gainNode.gain.setTargetAtTime(0, time + 0.1)
+  }
+
   return {
     connect,
+    stop,
     inputs: [
       {
         type: 'buffer',
